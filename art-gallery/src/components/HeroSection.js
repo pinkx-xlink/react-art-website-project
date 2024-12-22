b0cf6b41f6cd3d77e818f57780a85489fc14b8ca
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-12-22 16:35:52 -0500

    Update welcome text on Home page

--- art-gallery/src/components/HeroSection.js
+++ art-gallery/src/components/HeroSection.js
@@ -3,12 +3,13 @@ import './HeroSection.css';
 import '../App.css';
 import { Button } from './Button';
 
+// This is the welcome page
 function HeroSection() {
   return (
     <div className='hero-container'>
         <video src='/videos/video-1.mp4' autoPlay loop muted />
-        <h1>ADVENTURE AWAITS</h1>
-        <p> What are you waiting for? </p>
+        <h1><i>"Artists aren't created, they are born."</i></h1>
+        <p> What will you create? </p>
         <div className="hero-btns">
             <Button 
             className='btns' 
